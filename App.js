d4dae624cba804dcc58d725cb8c727b42d91d52c
Njiraini23 <njiraini88@gmail.com>
2024-09-14 18:56:05 +0300

    Learning React Hooks Fundamentals

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import DocTiteOne from './components/DocTiteOne';
+import DocTiteTwo from './components/DocTitleTwo';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <DocTiteOne />
+      <DocTiteTwo />
+    </div>
+  );
+}
+
+export default App;
